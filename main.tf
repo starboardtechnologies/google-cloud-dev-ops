cd55e5eb81eef94d8499bea263c576bc9b56e690
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-15 05:11:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,6 @@
-
+provider "google" {
+  credentials   = "${file("service-account.json")}"
+  project       = var.google_cloud_project
+  region        = var.google_cloud_project_region
+  zone          = var.google_cloud_project_zone
+}
