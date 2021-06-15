e37399a94c85a9a4b52bf43a74421655552c8aa4
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-15 05:12:17 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+variable "google_cloud_project" {
+  type = string
+  default = <INSERT GOOGLE CLOUD PROJECT NAME>
+}
 
+variable "google_cloud_project_region" {
+  type = string
+  default = <INSERT GOOGLE CLOUD PROJECT REGION>
+}
+
+variable "google_cloud_project_zone" {
+  type = string
+  default = <INSERT GOOGLE CLOUD PROJECT ZONE>
+}
