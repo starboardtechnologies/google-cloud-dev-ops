f935674d83feea9ba0e7fe6ea508030ed9c9a264
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-23 14:22:36 -0500

    Update google_compute_instance.tf

--- Essentials/Compute Engine/google_compute_instance.tf
+++ Essentials/Compute Engine/google_compute_instance.tf
@@ -1,1 +1,19 @@
+resource "google_compute_instance" "appservers" {
+    count         = 3
+    name          = var.dev_ops_compute_engine_name[count.index]
+    zone          = var.dev_ops_compute_engine_zone[count.index]
+    machine_type  = var.dev_ops_compute_engine_machine_machine_type[count.index]
 
+    boot_disk {
+    initialize_params {
+      image       = var.compute_engine_boot_disk_image[count.index]
+    }
+  }
+
+     network_interface {
+    network       = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
