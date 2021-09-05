dbc4bf2c830984821bb9898213b0b249fccfea40
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-09-05 15:30:29 -0500

    Update devops_google_cloud_app_engine.tf

--- DevOps/App Engine/devops_google_cloud_app_engine.tf
+++ DevOps/App Engine/devops_google_cloud_app_engine.tf
@@ -1,1 +1,12 @@
+resource "google_project" "devops_google_cloud_app_engine_project" {
+  count      = 3  
+  name       = var.devops_google_cloud_app_engine_project_name
+  project_id = var.devops_google_cloud_app_engine_project_id
+  org_id     = var.devops_google_cloud_app_engine_project_org_id
+}
 
+resource "google_app_engine_application" "devops_google_cloud_app_engine" {
+  count       = 3  
+  project     = var.devops_google_cloud_app_engine_project_id
+  location_id = var.devops_google_cloud_app_engine_location_id
+}
