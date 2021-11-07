0706a4c2e47c568fe80b087529dee716263c18e5
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-11-07 01:01:47 -0500

    Create google_cloud_service_account_iam_policy.tf

---
+++ Essentials/IAM/Service Accounts/google_cloud_service_account_iam_policy.tf
@@ -0,0 +1,4 @@
+resource "google_service_account_iam_policy" "admin-account-iam" {
+  service_account_id = google_service_account.sa.name
+  policy_data        = data.google_iam_policy.admin.policy_data
+}
